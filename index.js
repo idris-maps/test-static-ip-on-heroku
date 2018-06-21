25c0b85b105d8c549f966962613763d159a9d13a
Anders <idris.maps@gmx.ch>
2018-06-21 11:34:53 +0200

    Make it work locally

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const url = require('url')
 const app = express()
 const port = process.env.PORT || 3000
 const request = require('request')
 const ipifyUrl = 'https://api.ipify.org?format=json'
 
-
-const getOptions = (withProxy) => ({
-  proxy: withProxy ? process.env.QUOTAGUARDSTATIC_URL : undefined,
-  url: ipifyUrl,
-  headers: {
-      'User-Agent': 'node.js'
-  }
-})
 
 app.use(bodyParser.json())
 
@@ -22,16 +13,27 @@ app.get('/', (req, res) =>
   res.status(200).json({ message: 'The app works' }))
 
 app.get('/not-static', (req, res) => {
-  request(getOptions(false), (err, response, body) => res.status(200).json({
+  request({
+    url: ipifyUrl,
+    headers: {
+        'User-Agent': 'node.js'
+    }
+  }, (err, response, body) => res.status(200).json({
     code: response.statusCode,
-    body,
+    body: JSON.parse(body)
   }))
 })
 
 app.get('/static', (req, res) => {
-  request(getOptions(true), (err, response, body) => res.status(200).json({
+  request({
+    proxy: process.env.QUOTAGUARDSTATIC_URL,
+    url: ipifyUrl,
+    headers: {
+        'User-Agent': 'node.js'
+    }
+  }, (err, response, body) => res.status(200).json({
     code: response.statusCode,
-    body,
+    body: JSON.parse(body),
   }))
 })
 
