e3748ba55bf6d3e9cacf87b2817db5a3e734f24a
Anders <idris.maps@gmx.ch>
2018-06-21 09:52:39 +0200

    Add .gitignore

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const url = require('url')
 const axios = require('axios')
 const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
 
 const ipifyUrl = 'https://api.ipify.org?format=json'
 const fixieUrl = url.parse(process.env.FIXIE_URL)
@@ -13,10 +16,6 @@ const getServerIp = () => axios.get(ipifyUrl, {
 app.get('/', (req, res) => getServerIp()
   .then(({ip}) => res.status(200).json({ serverIp: ip })))
   .catch(err => res.status(500).json({ err }))
-
-app.use(bodyParser.json())
-
-const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Listening on ${port}...`)
