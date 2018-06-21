5ac50ddaf5a4ef68df6cc7dadda56edaaf8d2680
Anders <idris.maps@gmx.ch>
2018-06-21 11:06:54 +0200

    Use quotaguard

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const url = require('url')
@@ -8,14 +9,17 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.json())
 
 const ipifyUrl = 'https://api.ipify.org?format=json'
-const fixieUrl = url.parse(process.env.FIXIE_URL)
-const getServerIp = () => axios.get(ipifyUrl, {
-  proxy: { host: fixieUrl.host, port: fixieUrl.port }
-})
+const quotaGuardUrl = url.parse(process.env.QUOTAGUARDSTATIC_URL)
+const getServerIp = () =>
+  axios.get(ipifyUrl, {
+    proxy: { host: quotaGuardUrl.host, port: quotaGuardUr.port }
+  })
+    .then(res => res.data)
+
 
 app.get('/', (req, res) => getServerIp()
-  .then(({ip}) => res.status(200).json({ serverIp: ip })))
-  .catch(err => res.status(500).json({ err }))
+  .then(({ip}) => res.status(200).json({ serverIp: ip }))
+  .catch(err => res.status(500).json({ err })))
 
 app.listen(port, () => {
   console.log(`Listening on ${port}...`)
