8e20d84b8ff395b27174f3677b1a34cebb361d9d
Anders <idris.maps@gmx.ch>
2018-06-21 09:51:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const url = require('url')
+const axios = require('axios')
+const app = express()
+
+const ipifyUrl = 'https://api.ipify.org?format=json'
+const fixieUrl = url.parse(process.env.FIXIE_URL)
+const getServerIp = () => axios.get(ipifyUrl, {
+  proxy: { host: fixieUrl.host, port: fixieUrl.port }
+})
+
+app.get('/', (req, res) => getServerIp()
+  .then(({ip}) => res.status(200).json({ serverIp: ip })))
+  .catch(err => res.status(500).json({ err }))
+
+app.use(bodyParser.json())
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}...`)
+})
+
